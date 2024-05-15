142cea094a25f069e2aaeecf99681091c31d225e
Yuki Maruyama <maruyama_y@1923.co.jp>
2024-05-15 16:48:08 +0900

    Update: web page

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 document.getElementById('inputForm').addEventListener('submit', async function(event) {
   event.preventDefault();
-  const userInput = document.getElementById('script').value;
+  const userInput = document.getElementById('scriptForm').value;
   const req = new Request('/run', {method: "POST", body: userInput})
   const res = await fetch(req)
   document.getElementById('outputDisplay').textContent = await res.text()
