915d007dfdcc1c0f47ea44a4e6a5781add24715e
Yuki Maruyama <maruyama_y@1923.co.jp>
2024-05-16 11:34:55 +0900

    Update: loading text

--- public/script.js
+++ public/script.js
@@ -1,7 +1,15 @@
 document.getElementById('inputForm').addEventListener('submit', async function(event) {
   event.preventDefault();
+  document.getElementById('outputDisplay').textContent = "Wait ..."
   const userInput = document.getElementById('scriptForm').value;
   const req = new Request('/run', {method: "POST", body: userInput})
-  const res = await fetch(req)
-  document.getElementById('outputDisplay').textContent = await res.text()
+  fetch(req)
+    .then(async(res) => {
+      if (res.status != 200){
+        document.getElementById('outputDisplay').style.color = "red"
+      } else {
+        document.getElementById('outputDisplay').style.color = "black"
+      }
+      document.getElementById('outputDisplay').textContent = await res.text()
+    })
 });
